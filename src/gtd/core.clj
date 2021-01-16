7be3442204bf4d0d23e04a54b6b1eec7d9bdedc5
alexpetrov <alexpetrov.rb@gmail.com>
2021-01-16 03:27:07 +0300

    Render tune features guitarist and guitar

--- src/gtd/core.clj
+++ src/gtd/core.clj
@@ -29,6 +29,12 @@
                         :feature/kind [:feature-kind/id "guitar"]
                         :feature/description "First Leo Fender's solid body guitar"}
 
+                       {:db/id -201
+                        :feature/id "stratocaster"
+                        :feature/title "Stratocaster"
+                        :feature/kind [:feature-kind/id "guitar"]
+                        :feature/description "Second Leo Fender's solid body guitar"}
+
                        {:db/id -250
                         :feature/id "robben_ford"
                         :feature/title "Robben Ford"
@@ -49,7 +55,7 @@
                         :tune/discogs "https://www.discogs.com/ru/David-Sanborn-Hearsay/release/2459059"
                         :tune/comment "First guitar tone I got obsessed with when I was 14"
                         :tune/features [[:feature/id "robben_ford"]
-                                        [:feature/id "telecaster"]]}
+                                        [:feature/id "stratocaster"]]}
 
                        {:db/id -400
                         :tune/id "blues_for_narada"
@@ -57,7 +63,8 @@
                         :tune/band "Gary Moore"
                         :tune/youtube "https://www.youtube.com/embed/EntSKjMghnI"
                         :tune/comment "Second guitar tone I got obsessed with when I was 15"
-                        :tune/features [[:feature/id "gary_moore"]]}] )
+                        :tune/features [[:feature/id "gary_moore"]
+                                        [:feature/id "telecaster"]]}] )
 
     @conn))
 
@@ -72,7 +79,21 @@
        (map #(tune-with-features db (first %1)))
        (reverse)))
 
+
+(defn feature [db feature-kind-id tune-id]
+  (let [feature-db-ids (d/q '[:find ?feature-id
+                              :in $ ?fkid ?tid
+                              :where
+                              [?tid :tune/features ?feature-id]
+                              [?feature-id :feature/kind ?fkid]
+                              ]
+                            db [:feature-kind/id feature-kind-id] [:tune/id tune-id])]
+    (d/pull db '[*] (ffirst feature-db-ids))))
+
+;; (def test-data [{:db/id 5, :feature/description "Gary Moore //TBD", :feature/id "gary_moore", :feature/kind {:db/id 2, :feature-kind/description "Musician playing on guitar in the tune", :feature-kind/id "guitarist"}, :feature/title "Gary Moore"}])
+
 ;; (all-tunes (db))
+;;(:tune/features (first (all-tunes (db))))
 ;;
 ;; (d/pull (db) '[:feature/title {:feature/kind [:db/id :feature-kind/title]}] [:feature/id "telecaster"])
 ;; (d/pull (db) '[:tune/title :tune/id {:tune/features [* {:feature/kind [*]}]}] [:tune/id "little_face"])
@@ -83,17 +104,20 @@
 
 (def tmpl "templates/gtd.html")
 
-;; (html/deftemplate tune-features tmpl [:div.tune-features]
-;;   [features]
-;;   )
+(html/defsnippet tune-features tmpl [:div.tune-features]
+  [tune-id]
+  [:span.guitarist] (html/content (:feature/title (feature (db) "guitarist" tune-id)))
+  [:span.guitar] (html/content (:feature/title (feature (db) "guitar" tune-id)))
+  )
 
 (html/defsnippet tune tmpl [:div.tune]
-  [{:keys [tune/title tune/band tune/comment tune/youtube tune/discogs]}]
-  [:.title] (html/html-content title)
-  [:.band] (html/html-content band)
-  [:.comment] (html/html-content comment)
+  [{:keys [tune/title tune/band tune/comment tune/youtube tune/discogs tune/features tune/id]}]
+  [:.title] (html/content title)
+  [:.band] (html/content band)
+  [:.comment] (html/content comment)
   [:iframe.youtube] (html/set-attr :src youtube)
   [:.discogs] (if (nil? discogs) nil (html/set-attr :src discogs))
+  [:.tune-features] (html/content (tune-features id))
   )
 
 ;; (-main)
