70771e379192c3f405735d720722daeffbf24689
alexpetrov <alexpetrov.rb@gmail.com>
2021-01-30 19:25:16 +0300

    Autogenerate db-ids and extract facts to a file

--- src/gtd/core.clj
+++ src/gtd/core.clj
@@ -6,6 +6,13 @@
   (->> (d/entity db id)
        (into {})))
 
+(defn facts [] (read-string (slurp "resources/data/facts.edn")))
+
+(defn facts-with-ids [facts]
+  (map
+   (fn [[fact id]] (assoc fact :db/id (inc id)))
+   (partition 2 (interleave facts (range)))))
+
 (defn db []
   (let [schema {:tune/id {:db/unique :db.unique/identity}
                 :feature/id {:db/unique :db.unique/identity}
@@ -15,58 +22,7 @@
                 :tune/features {:db/cardinality :db.cardinality/many :db/valueType :db.type/ref}
                 }
         conn (d/create-conn schema)]
-    (d/transact! conn [{:db/id -100
-                        :feature-kind/id "guitar"
-                        :feature-kind/title "Guitar"
-                        :feature-kind/description "Kind of guitar"}
-
-                       {:db/id -101
-                        :feature-kind/id "guitarist"
-                        :feature-kind/title "Guitarist"
-                        :feature-kind/description "Musician playing on guitar in the tune"}
-
-                       {:db/id -200
-                        :feature/id "telecaster"
-                        :feature/title "Telecaster"
-                        :feature/kind [:feature-kind/id "guitar"]
-                        :feature/description "First Leo Fender's solid body guitar"}
-
-                       {:db/id -201
-                        :feature/id "stratocaster"
-                        :feature/title "Stratocaster"
-                        :feature/kind [:feature-kind/id "guitar"]
-                        :feature/description "Second Leo Fender's solid body guitar"}
-
-                       {:db/id -250
-                        :feature/id "robben_ford"
-                        :feature/title "Robben Ford"
-                        :feature/kind [:feature-kind/id "guitarist"]
-                        :feature/description "Robben Ford //TBD"}
-
-                       {:db/id -251
-                        :feature/id "gary_moore"
-                        :feature/title "Gary Moore"
-                        :feature/kind [:feature-kind/id "guitarist"]
-                        :feature/description "Gary Moore //TBD"}
-
-                       {:db/id -300
-                        :tune/id "little_face"
-                        :tune/title "Little Face"
-                        :tune/band "David Sanborn"
-                        :tune/youtube "https://www.youtube.com/embed/eGAgpn_m-ZE?start=170"
-                        :tune/discogs "https://www.discogs.com/ru/David-Sanborn-Hearsay/release/2459059"
-                        :tune/comment "First guitar tone I got obsessed with when I was 14"
-                        :tune/features [[:feature/id "robben_ford"]
-                                        [:feature/id "telecaster"]]}
-
-                       {:db/id -400
-                        :tune/id "blues_for_narada"
-                        :tune/title "Blues For Narada"
-                        :tune/band "Gary Moore"
-                        :tune/youtube "https://www.youtube.com/embed/EntSKjMghnI"
-                        :tune/comment "Second guitar tone I got obsessed with when I was 15"
-                        :tune/features [[:feature/id "gary_moore"]
-                                        [:feature/id "telecaster"]]}] )
+    (d/transact! conn (facts-with-ids (facts)))
 
     @conn))
 
